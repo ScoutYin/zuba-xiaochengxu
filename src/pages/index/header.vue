e5e41cb5bafdf0348d6f0ab13771d1209772730f
ScoutYin <yinbin_web@163.com>
2018-12-31 00:11:31 +0800

    Initial commit

---
+++ src/pages/index/header.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="home-header">
+  <div class="home-header-location">
+    
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style>
+
+</style>
